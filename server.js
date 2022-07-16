bbb8c9e91a9f0b3dfffc547c53bff6e18debd4c6
BabaScience <lebabamath@gmail.com>
2022-07-16 17:30:30 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+
+const PORT = 5000
+const app = express()
+
+
+
+
+
+app.get('/', (req, res) => {
+	res.send("Hello World")
+
+})
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`listening on PORT=${PORT}`)
+})
